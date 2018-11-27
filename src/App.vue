71d6a8be7c5e074b1af691d9ecef5683d13d0dfb
yangguoyu <516433517@qq.com>
2018-11-27 13:32:11 +0800

    initsell

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <router-view/>
+    <foot v-show="$route.meta.showFooter"/>
+  </div>
+</template>
+
+<script>
+  import foot from 'components/foot/foot'
+
+  export default {
+    components: {
+      foot
+    }
+  }
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+  .app
+    width:100%
+    height: 100%
+    background:#f5f5f5
+</style>
